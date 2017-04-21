36a2ca68101e395c21da5240272ee64f45e33452
Munir Contractor <mmc691@nyu.edu>
2017-04-21 18:32:46 -0400

    Combined various shell commands to make scripts smaller

--- scripts/setup-dnsmasq.sh
+++ scripts/setup-dnsmasq.sh
@@ -11,8 +11,7 @@ systemctl stop dnsmasq                                       &&
 install -v -m 644 etc/systemd/system/{dnsmasq,fix-ethernet-issue}.service \
                   /etc/systemd/system/                       &&
 install -v -m 755 scripts/fix-ethernet-issue /usr/local/bin/ &&
-install -v -m 644 etc/resolv.dnsmasq.conf /etc/              &&
-install -v -m 644 etc/dnsmasq.conf /etc/                     &&
+install -v -m 644 etc/{resolv.,}dnsmasq.conf /etc/           &&
 install -v -m 644 etc/systemd/network/20-ethernet-dnsmasq.network \
                   /etc/systemd/network/10-ethernet.network   &&
 touch /etc/dnsmasq_hosts
@@ -22,7 +21,6 @@ echo "DNS resolution. If you wish to use different DNS servers, please"
 echo "/etc/resolv.dnsmasq.conf"
 echo "Trying to start dnsmasq...in case of errors, please make sure that"
 echo "no other service is overwriting /etc/resolv.conf"
-systemctl daemon-reload             &&
-systemctl enable dnsmasq            &&
-systemctl enable fix-ethernet-issue &&
+systemctl daemon-reload                                      &&
+systemctl enable dnsmasq fix-ethernet-issue                  &&
 systemctl start dnsmasq
--- scripts/switch-to-systemd-networkd.sh
+++ scripts/switch-to-systemd-networkd.sh
@@ -5,21 +5,12 @@ if [[ $EUID > 0 ]]; then
     exit
 fi
 
-systemctl disable NetworkManager                               &&
-systemctl disable dhcpcd5                                      &&
-systemctl disable networking                                   &&
-systemctl disable wpa_supplicant                               &&
-systemctl enable systemd-networkd                              &&
-systemctl enable systemd-resolved                              &&
-install -v -d -m 755 /etc/systemd/network/                     &&
-install -v -m 644 etc/systemd/network/10-ethernet.network \
-                  /etc/systemd/network/10-ethernet.network     &&
-install -v -m 644 etc/systemd/network/10-wireless.network \
-                  /etc/systemd/network/10-wireless.network     &&
-install -v -m 644 etc/systemd/system/wpa_supplicant@.service \
-                  /etc/systemd/system/wpa_supplicant@.service  &&
-systemctl daemon-reload                                        &&
-systemctl enable wpa_supplicant@wlan0
-echo "Successfully switched to systemd-networkd. Ethernet and wireless configured as DHCP" &&
+systemctl disable NetworkManager dhcpcd5 networking wpa_supplicant                 &&
+install -v -d -m 755 /etc/systemd/network/                                         &&
+install -v -m 644 etc/systemd/network/10-*.network           /etc/systemd/network/ &&
+install -v -m 644 etc/systemd/system/wpa_supplicant@.service /etc/systemd/system/  &&
+systemctl daemon-reload                                                            &&
+systemctl enable systemd-network systemd-resolved wpa_supplicant@wlan0
+echo "Successfully switched to systemd-networkd. Ethernet and wireless configured as DHCP"
 echo "Network files are located in /etc/systemd/network/"
 echo "systemd-networkd will be used on reboot"
