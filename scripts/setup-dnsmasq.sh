d6b097a59d56f3c772de53f09338722dd59bda09
Munir Contractor <mmc691@nyu.edu>
2017-04-21 17:32:56 -0400

    Added ethernet issue fixes to dnsmasq setup script

--- scripts/setup-dnsmasq.sh
+++ scripts/setup-dnsmasq.sh
@@ -5,13 +5,16 @@ if [[ $EUID > 0 ]] ; then
     exit
 fi
 
-apt-get -y install dnsmasq                             &&
-systemctl disable dnsmasq                              &&
-systemctl stop dnsmasq                                 &&
-install -v -m 644 etc/systemd/system/dnsmasq.service \
-                  /etc/systemd/system/dnsmasq.service  &&
-install -v -m 644 etc/resolv.dnsmasq.conf /etc/        &&
-install -v -m 644 etc/dnsmasq.conf /etc/               &&
+apt-get -y install dnsmasq                                   &&
+systemctl disable dnsmasq                                    &&
+systemctl stop dnsmasq                                       &&
+install -v -m 644 etc/systemd/system/{dnsmasq,fix-ethernet-issue}.service \
+                  /etc/systemd/system/                       &&
+install -v -m 755 scripts/fix-ethernet-issue /usr/local/bin/ &&
+install -v -m 644 etc/resolv.dnsmasq.conf /etc/              &&
+install -v -m 644 etc/dnsmasq.conf /etc/                     &&
+install -v -m 644 etc/systemd/network/20-ethernet-dnsmasq.network \
+                  /etc/systemd/network/10-ethernet.network   &&
 touch /etc/dnsmasq_hosts
 echo "dnsmasq setup successfully"
 echo "Configured to use public DNS servers from Google for upstream"
@@ -19,6 +22,7 @@ echo "DNS resolution. If you wish to use different DNS servers, please"
 echo "/etc/resolv.dnsmasq.conf"
 echo "Trying to start dnsmasq...in case of errors, please make sure that"
 echo "no other service is overwriting /etc/resolv.conf"
-systemctl daemon-reload  &&
-systemctl enable dnsmasq &&
+systemctl daemon-reload             &&
+systemctl enable dnsmasq            &&
+systemctl enable fix-ethernet-issue &&
 systemctl start dnsmasq
