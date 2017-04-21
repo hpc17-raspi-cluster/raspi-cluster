9988477c1b52cb1047d2b122b834714b7bdb0225
Munir Contractor <mmc691@nyu.edu>
2017-04-21 13:51:53 -0400

    Disabled all Debian Jessie networking madness (dhcpcd5 and networking service)

--- scripts/switch-to-systemd-networkd.sh
+++ scripts/switch-to-systemd-networkd.sh
@@ -6,13 +6,13 @@ if [[ $EUID > 0 ]]; then
 fi
 
 systemctl disable NetworkManager  &&
+systemctl disable dhcpcd5         &&
+systemctl disable networking      &&
 systemctl enable systemd-networkd &&
 systemctl enable systemd-resolved &&
-systemctl start systemd-resolved  &&
-rm -vf /etc/resolv.conf           &&
-ln -vs /run/systemd/resolve/resolv.conf /etc/resolv.conf &&
 install -v -d -m 755 /etc/systemd/network/ &&
 install -v -m 644 etc/systemd/network/10-ethernet.network /etc/systemd/network/10-ethernet.network &&
 install -v -m 644 etc/systemd/network/10-wireless.network /etc/systemd/network/10-wireless.network &&
 echo "Successfully switched to systemd-networkd. Ethernet and wireless configured as DHCP" &&
 echo "Network files are located in /etc/systemd/network/"
+echo "systemd-networkd will be used on reboot"
