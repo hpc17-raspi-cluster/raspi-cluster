e4566d48213fa188f462242f74a10a556c47bbd8
Munir Contractor <mmc691@nyu.edu>
2017-04-24 22:51:14 -0400

    Changed options for mounting to be more suitable for /home use case

--- scripts/nfs-mount-home.sh
+++ scripts/nfs-mount-home.sh
@@ -7,6 +7,6 @@ fi
 
 mkdir -pv /mnt/usb                  &&
 mount -v 192.168.1.1:/home /mnt/usb &&
-echo "192.168.1.1:/home /home nfs noauto,x-systemd.automount,x-systemd.device-timeout=10,timeo=14,x-systemd.idle-timeout=1min 0 0" >> /etc/fstab
+echo "192.168.1.1:/home /home nfs auto,nofail,x-systemd.automount,x-systemd.device-timeout=10,timeo=110 0 0" >> /etc/fstab
 echo "Updated /etc/fstab to mount /home from network"
 umount -v /mnt/usb
