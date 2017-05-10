e777d388815a10bed66ed581cc02928c5f77b1ad
evcu <evcu@users.noreply.github.com>
2017-05-10 00:22:19 -0400

    fixs

--- benchmarks/slurm_submit script.sh
+++ benchmarks/slurm_submit script.sh
@@ -9,8 +9,8 @@ read -d '' text << EOF
 #SBATCH -J hpcclass           # Job name
 #SBATCH -o %j.$1.out       # Name of stdout output file (%j expands to jobId)
 #SBATCH -p normal        # Queue name
-#SBATCH -N 16                  # Total number of nodes requested (16 cores/node)
-#SBATCH -n 256                 # Total number of mpi tasks requested
+#SBATCH -N $2                  # Total number of nodes requested (16 cores/node)
+#SBATCH -n $((${2}*16))                 # Total number of mpi tasks requested
 #SBATCH -t 00:30:00           # Run time (hh:mm:ss) - 1.5 hours
 
 #SBATCH -A TG-ASC170005      # <-- Allocation name to charge job against
